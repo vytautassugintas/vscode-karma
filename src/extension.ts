7f508adad8370dea1433e8c433a54328543a6b37
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2018-01-03 21:46:19 +0200

    feat: exec npm script and listen for its ouput. Also text in file decoration test

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,103 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import {window, commands, workspace, Range, OverviewRulerLane, DecorationOptions, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
+import * as cp from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
+    const p = cp.exec('npm run test');
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "karma-inline-tests-results" is now active!');
+		p.stderr.on('data', (chunk: string) => {
+            console.log("----got some data stderr");
+            console.log(chunk);
+        });
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+		p.stdout.on('data', (chunk: string) => {
+            console.log("----got some data stdout");
+            console.log(chunk);   
+        });
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+		p.on('close', (_code: number, _signal: string) => {
+			try {
+                console.log("Closed");
+				console.log(_code);   
+			} catch (e) {
+				console.log(e);
+			}
+		});
+	
+	// create a decorator type that we use to decorate small numbers
+	const smallNumberDecorationType = window.createTextEditorDecorationType({
+		borderWidth: '1px',
+		borderStyle: 'solid',
+		overviewRulerColor: 'blue',
+		overviewRulerLane: OverviewRulerLane.Right,
+		light: {
+			// this color will be used in light color themes
+			borderColor: 'darkblue'
+		},
+		dark: {
+			// this color will be used in dark color themes
+			borderColor: 'lightblue'
+		}
+	});
 
-    context.subscriptions.push(disposable);
+	// create a decorator type that we use to decorate large numbers
+	const largeNumberDecorationType = window.createTextEditorDecorationType({
+		cursor: 'crosshair',
+		backgroundColor: 'rgba(255,0,0,0.3)'
+	});
+
+	let activeEditor = window.activeTextEditor;
+	if (activeEditor) {
+		triggerUpdateDecorations();
+	}
+
+	window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		if (editor) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	var timeout = null;
+	function triggerUpdateDecorations() {
+		if (timeout) {
+			clearTimeout(timeout);
+		}
+		timeout = setTimeout(updateDecorations, 500);
+	}
+
+	function updateDecorations() {
+		if (!activeEditor) {
+			return;
+		}
+		const regEx = /(test|it)/g;
+		const text = activeEditor.document.getText();
+		const smallNumbers: DecorationOptions[] = [];
+		const largeNumbers: DecorationOptions[] = [];
+		let match;
+		while (match = regEx.exec(text)) {
+			const startPos = activeEditor.document.positionAt(match.index);
+			const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+			const decoration = { range: new Range(startPos, endPos), hoverMessage: 'Number **' + match[0] + '**' };
+			if (match[0].length < 3) {
+				smallNumbers.push(decoration);
+            } else {
+				largeNumbers.push(decoration);
+			}
+		}
+		activeEditor.setDecorations(smallNumberDecorationType, smallNumbers);
+		activeEditor.setDecorations(largeNumberDecorationType, largeNumbers);
+	}
 }
 
 // this method is called when your extension is deactivated
